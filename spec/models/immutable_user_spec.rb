e0c6e0f09be8bfe9f1201f9ec9a1fb6bc7e2233b
himanoa <matsunoappy@gmail.com>
2019-07-24 06:25:29 +0900

    (test-red) Add test case
    - Don't run UPDATE query when target is saved record

--- spec/models/immutable_user_spec.rb
+++ spec/models/immutable_user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ImmutableUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#updaete" do
+    context 'when not saved record' do
+      let(:user) { ImmutableUser.new(name: 'himanoa') }
+      it { expect { user.update(name: 'h1manoa').to raise_error ActiveRecord::ReadOnlyRecord} }
+    end
+    context 'when saved record' do
+      let(:user) { ImmutableUser.create(name: 'himanoa') }
+      it { expect { user.update(name: 'h1manoa').to raise_error ActiveRecord::ReadOnlyRecord} }
+    end
+  end
 end
