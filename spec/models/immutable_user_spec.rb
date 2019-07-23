b9287cb8ef14c8ea32318eede5a55b40e91c37ec
himanoa <matsunoappy@gmail.com>
2019-07-24 07:19:30 +0900

    Fix test

--- spec/models/immutable_user_spec.rb
+++ spec/models/immutable_user_spec.rb
@@ -1,14 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ImmutableUser, type: :model do
-  describe "#updaete" do
+  before(:each) do
+    ImmutableUser.delete_all
+  end
+
+  describe '#updaete' do
     context 'when not saved record' do
       let(:user) { ImmutableUser.new(name: 'himanoa') }
-      it { expect { user.update(name: 'h1manoa').to raise_error ActiveRecord::ReadOnlyRecord} }
+      it { expect { user.update(name: 'h1manoa') } }
     end
     context 'when saved record' do
       let(:user) { ImmutableUser.create(name: 'himanoa') }
-      it { expect { user.update(name: 'h1manoa').to raise_error ActiveRecord::ReadOnlyRecord} }
+      it { expect { user.update(name: 'h1manoa') }.to raise_error ActiveRecord::ReadOnlyRecord }
     end
   end
 end
